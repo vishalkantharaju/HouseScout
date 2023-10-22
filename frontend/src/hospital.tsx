daee2a8c4bd8496d5e7bf1132bf7b50dd0c1466a
jellybean-7 <angelicasharma@utexas.edu>
2023-10-22 05:27:10 -0500

    hospital

--- frontend/src/hospital.tsx
+++ frontend/src/hospital.tsx
@@ -29,14 +29,17 @@ export default function Hospital() {
     };
     return (
         <div className='max-w-screen max-h-screen'>
-            <div className="w-full h-16 bg-gradient-to-r from-[#DF059C] via-[#7749C1] to-[#00B1FF] fixed z-10">
-                <div className='flex items-center justify-start h-full pl-12'>
-                    <img className='w-20 h-12' src={ambulance}></img>
-                    <p className = 'font-bold text-2xl text-white'>
-                    MedHelp
-                    </p>
+            {/* Navbar */}
+            <div className="w-full border h-16 bg-gradient-to-r from-[#DF059C] via-[#7749C1] to-[#00B1FF] fixed">
+                <div className='flex items-center justify-between h-full pl-12 pr-4'>
+                    <div className='flex items-center'>
+                    <img className='w-20 h-12' src={ambulance} alt="Ambulance" />
+                    <p className='font-bold text-2xl text-white'>MedHelp</p>
+                    </div>
+                    <div className='font-bold text-2xl text-white pr-12'>Hospital</div>
                 </div>
             </div>
+            
             {/* <div className='h-16'></div> */}
             <div className='flex w-full h-full'>
                 <div className='border-2 w-1/3'>
