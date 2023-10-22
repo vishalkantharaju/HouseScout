d8f7e891bdddb6d3d8d71d2b79fcbb595e02b4b7
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2023-10-22 02:29:59 -0500

    pages linked

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import ambulance from './assets/medhelp_icon_transparent.png'
 import background from './assets/ambulance_background.jpg'
 import health_icon from './assets/Viva_Drawing.png'
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   // const [count, setCount] = useState(0)
+  const nav = useNavigate();
 
   return (
     <div className='max-w-screen max-h-screen'>
       <div className="w-full border h-16 bg-gradient-to-r from-[#DF059C] via-[#7749C1] to-[#00B1FF] fixed">
-        <div className='flex items-center justify-start h-full'>
-            <img className='w-22 h-14' src={ambulance}></img>
-            <p className = 'font-bold text-2xl text-white'>
+        <div className='flex items-center justify-start h-full pl-12'>
+              <img className='w-20 h-12' src={ambulance}></img>
+              <p className = 'font-bold text-2xl text-white'>
               MedHelp
-            </p>
-        </div>
+              </p>
+          </div>
         <div>
           <div className='bg-[url("assets/ambulance_background.jpg")] bg-cover bg-center bg-no-repeat w-full h-screen'>
             <div className="bg-[#06101E] bg-opacity-60 w-full h-screen flex">
@@ -26,7 +28,7 @@ function App() {
                     one patient at a time.
                   </span>
                 </div>
-                <div className="my-10 w-36 h-14 rounded-md bg-[#00B1FF] flex items-center justify-center cursor-pointer hover:bg-blue-700">
+                <div onClick={() => {nav('/login')}} className="my-10 w-36 h-14 rounded-md bg-[#00B1FF] flex items-center justify-center cursor-pointer hover:bg-blue-700">
                   <div className="text-black text-3xl font-bold text-center pb-1">Sign In</div>
                 </div>
               </div>
